e2e3cbdc130601ef360579fd18cafc62eba15b86
akirakurea <tjckdals92@gmail.com>
2026-09-15 15:25:04 +0900

    btadded
    add bt

---
+++ 3rd3DProject/Assets/02Scripts/02Enemy/BT_Leaf.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+public class BT_Leaf : BT_Node
+{
+    public Func<BT_NodeStatus> action;
+
+    public BT_Leaf(Func<BT_NodeStatus> action)
+    {
+        this.action = action;
+    }
+    public override BT_NodeStatus Evaluate()
+    {
+        return action();
+    }
+}
