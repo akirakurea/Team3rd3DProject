011d019a0e3d30660d5f2c432a94eca0433b90e6
akirakurea <tjckdals92@gmail.com>
2026-09-16 17:07:35 +0900

    BT 추가
    BT추가 및 컴파일 에러 처리용 플레이스 홀더 스크립트 추가

---
+++ 3rd3DProject/Assets/02Scripts/02Enemy/BT_StealItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BT_StealItem : BT_Leaf
+{
+    private ThiefBlackboard blackboard;
+    private float stealDuration = 2.5f;
+    private float currentTimer = 0f;
+    private bool isNotifying = false;
+
+    public BT_StealItem(ThiefBlackboard blackboard)
+    {
+        this.blackboard = blackboard;
+    }
+    public override BT_NodeStatus Evaluate()
+    {
+        if (blackboard.TargetItem == null) return BT_NodeStatus.Failure;
+
+        if(!isNotifying)//훔치기 시작
+        {
+            isNotifying = true;
+            //AlertSystem.TriggerStealAlert(blackboard.TargetItem.position);
+            //경보시스템 스크립트 필요
+        }
+
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= stealDuration)
+        {
+            ItemManager2.Instance.RemoveItem(blackboard.TargetItem);
+            blackboard.TargetItem = null;
+
+            currentTimer = 0f;
+            isNotifying = false;
+            return BT_NodeStatus.Success;
+
+
+        }
+        return BT_NodeStatus.Running;
+    }
+
+
+
+}
---
+++ 3rd3DProject/Assets/02Scripts/02Enemy/ThiefBlackboard.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ThiefBlackboard
+{
+    public Transform PlayerTransform;
+    public Transform TargetItem;
+}
