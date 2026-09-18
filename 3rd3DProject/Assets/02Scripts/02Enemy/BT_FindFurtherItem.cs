e6b1b0a75d864ff40a48133b84babd213ac7ffde
akirakurea <tjckdals92@gmail.com>
2026-09-18 17:52:25 +0900

    BT추가함
    으어어 그런데 왜 진우님꺼 여기 클론 안되지 ㅠ

--- 3rd3DProject/Assets/02Scripts/02Enemy/BT_FindFurtherItem.cs
+++ 3rd3DProject/Assets/02Scripts/02Enemy/BT_FindFurtherItem.cs
@@ -15,11 +15,14 @@ public class BT_FindFurtherItem : BT_Leaf
         if (items.Count == 0) return BT_NodeStatus.Failure;
 
         Transform player = blackboard.PlayerTransform;
+        if(player == null)  return BT_NodeStatus.Failure;
+
         Transform furherItem = null;
         float maxDistance = -1f;
 
         foreach(var item in items)
         {
+            if(item == null) continue;
             float distanceToPlayer = Vector3.Distance(item.transform.position, player.position);
             if(distanceToPlayer > maxDistance)
             {
---
+++ 3rd3DProject/Assets/02Scripts/02Enemy/ThiefController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ThiefController : MonoBehaviour
+{
+    [Header("AI 블랙보드")]
+    [SerializeField] private ThiefBlackboard blackboard = new ThiefBlackboard();
+
+    private BT_Node rootNode;
+
+    private void Awake()
+    {
+        blackboard.Initailize(gameObject);
+
+        
+        BuildBehaviorTree();
+    }
+
+    private void Update()
+    {
+        Debug.Log($"NavMesh 연결 상태: {blackboard.Agent.isOnNavMesh}");
+        blackboard.UpdatePerception();
+
+        if(rootNode != null )
+            rootNode.Evaluate();
+    }
+
+    private void BuildBehaviorTree()
+    {
+        BT_Selector rootSelector = new BT_Selector();
+
+        BT_StunCheck stunCheckNode = new BT_StunCheck(blackboard);//함정밟은 상태를 가장 먼저 체크하기
+        rootSelector.AddChild(stunCheckNode);
+
+        BT_Sequence stealSequnce = new BT_Sequence();
+
+        stealSequnce.AddChild(new BT_FindFurtherItem(blackboard));
+
+        //이 구간에는 행동트리 추가 가능
+        stealSequnce.AddChild(new BT_MoveToTarget(blackboard));
+
+        rootSelector.AddChild(stealSequnce);
+
+        rootNode = rootSelector;
+    }
+    public ThiefBlackboard Blackboard => blackboard;
+
+}
