51b9e3c3c346fa2db27a724efed32721aa5d4c82
akirakurea <tjckdals92@gmail.com>
2026-09-21 17:17:18 +0900

    아이템 훔치기 등 추가
    임시 아이템 매니저와 임시 아이템 스크립트 임시 아이템 프리팹 존재.
    테스트용으로 만든 것이니 참조하셔도 됩니다. 현재 테스트용 씬에서 아이템으로 향하거나 플레이어로부터 도망가는 등이 구현되어있습니다.

--- 3rd3DProject/Assets/02Scripts/02Enemy/BT_FindFurtherItem.cs
+++ 3rd3DProject/Assets/02Scripts/02Enemy/BT_FindFurtherItem.cs
@@ -12,29 +12,45 @@ public class BT_FindFurtherItem : BT_Leaf
     public override BT_NodeStatus Evaluate()
     {
         var items = ItemManager2.Instance.GetRemainingItems();
-        if (items.Count == 0) return BT_NodeStatus.Failure;
+        if (items == null || items.Count == 0)
+        {
+            Debug.LogWarning("[BT] 아이템매니저 비어있음");
+            return BT_NodeStatus.Failure;
+        }
 
         Transform player = blackboard.PlayerTransform;
-        if(player == null)  return BT_NodeStatus.Failure;
+        Transform thief = blackboard.ThiefTransform;
+        if (player == null)
+        {
+            Debug.LogWarning("[BT] 플레이어 비어있음");
+            return BT_NodeStatus.Failure;
+        }
 
         Transform furherItem = null;
         float maxDistance = -1f;
-
+       
         foreach(var item in items)
         {
             if(item == null) continue;
             float distanceToPlayer = Vector3.Distance(item.transform.position, player.position);
-            if(distanceToPlayer > maxDistance)
+            float distanceToThiedf = Vector3.Distance(item.transform.position, thief.position);
+
+            //플레이어와 멀수록 점수가 올라감 && 도둑과 가까울수록 점수가 더 크게 올라감
+            float score = distanceToPlayer / (distanceToThiedf + 0.1f); 
+
+            if(score > maxDistance)
             {
-                maxDistance = distanceToPlayer;
+                maxDistance = score;
                 furherItem = item.transform; 
             }
         }
         if(furherItem != null)
         {
             blackboard.TargetItem = furherItem;
+            Debug.Log($"[BT]아이템 찾기 성공 : {furherItem.name}");
             return BT_NodeStatus.Success;
         }
+        Debug.LogWarning($"[BT]아이템 찾기 실패(리스트 or Player Null");
         return BT_NodeStatus.Failure;
     }
 }
